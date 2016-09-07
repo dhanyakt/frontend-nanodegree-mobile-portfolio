0cc333b8fdb913805f4480f5b09007732444cd68
Dhanya <dhanyakt@gmail.com>
2016-09-07 11:15:38 -0400

    Added the ngrok and pagespeed to view from local environment

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use-strict';
+
+var ngrok = require('ngrok');
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold:30
+            },
+            local: {
+                options: {
+                    strategy: "desktop"
+                }
+            },
+            mobile: {
+                options: {
+                    strategy: "mobile"
+                }
+            },
+        },
+    });
+
+    grunt.registerTask('psi-ngrok','Run pagespeed with ngrok', function() {
+        var done = this.async();
+        var port = 8000;
+        ngrok.connect(port, function(err,url){
+            if(err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+
+            grunt.config.set('pagespeed.options.url', url);
+            grunt.task.run('pagespeed');
+            done();
+        });
+    });
+
+    grunt.registerTask('default',['psi-ngrok']);
+};
